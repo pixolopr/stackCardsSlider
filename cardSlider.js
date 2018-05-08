c47357decfdb7bb2e3462c62372647214d325237
unknown <abhaya@DSKHP_LP6.WEB-18.COM>
2018-05-08 15:11:09 +0530

    Controls for height, width, animation direction and Animation properties

--- cardSlider.js
+++ cardSlider.js
@@ -1,78 +1,89 @@
 $.fn.accordion = function () {
 
     $(this).addClass("card-slider-wrapper");
-    
+
     var wrapperWidth = $(this).width();
     var numberOfItems = $(this).children("li").length;
-    
-    
 
-    var a = $(this);
+    //MAX WIDTH OF A SINGLE ITEM WILL BE THE TOTAL WIDTH MINUS THE SPACE NEEDED FOR THE REMAINING CADS TO HAVE A MINIMUM BREATHING SPACE
+    var minBreathingSpace = 15;
+    var maxPossibleWidth = wrapperWidth - (minBreathingSpace * (numberOfItems - 1));
+
+    //DEFINE VARIABLES
+    var animationTime = 700;
+    var slideDelay = 3000;
+    var slideDirection = "left";
+
+    var sliderWrapper = $(this);
     var c = 5;
     var e = null;
-    
+
     var widestItem = 0;
     var tallestItem = 0;
-    a.children("li").each(function (f) {
+    sliderWrapper.children("li").each(function (f) {
         var itemWidth = $(this).width();
         var itemHeight = $(this).height();
-        if(itemWidth > widestItem){
+        if (itemWidth > widestItem) {
             widestItem = itemWidth;
         };
-        if(itemHeight > tallestItem){
+        if (itemHeight > tallestItem) {
             tallestItem = itemHeight;
         };
     });
-    
+
+    if (widestItem > maxPossibleWidth) {
+        widestItem = maxPossibleWidth;
+    };
+
     $(this).css('width', '100%;');
-    $(this).css('height', (tallestItem+100)+"px");
-    
-    var divideCardsWidth = wrapperWidth-widestItem;
-    var cardToDivide = numberOfItems-1;
-    var perCardShowWidth = divideCardsWidth/cardToDivide;
-    var widthToSlide = widestItem-perCardShowWidth;
-    console.log(perCardShowWidth);
-    
-    a.children("li").each(function (f) {
-        $(this).css({
-            "width": widestItem,
-            "left": f * perCardShowWidth,
-            "zIndex": 20 - f
-        });
+    $(this).css('height', (tallestItem + 100) + "px");
+
+    var divideCardsWidth = wrapperWidth - widestItem;
+    var cardToDivide = numberOfItems - 1;
+    var perCardShowWidth = divideCardsWidth / cardToDivide;
+    var widthToSlide = widestItem - perCardShowWidth;
+
+    console.log(widestItem);
+
+    sliderWrapper.children("li").each(function (f) {
+        var cssObj = {};
+        cssObj["width"] = widestItem;
+        cssObj[slideDirection] = f * perCardShowWidth;
+        cssObj["zIndex"] = 20 - f;
+
+        $(this).css(cssObj);
     });
 
-    function b(f) {
-        //if ()
-        if (f != 0) {
-            var left = parseInt($('.card-slider-wrapper').children("li")[f - 1].style.left);
+    function slideToCard(cardIndex) {
+        if (cardIndex != 0) {
+            var cardSlideVal = parseInt($('.card-slider-wrapper').children("li")[cardIndex - 1].style[slideDirection]);
 
 
-            //IS THE CARD BEFORE THIS IN ITS LEFT MOST POSITION OR NOT
-            if (left > (((f - 1) * perCardShowWidth) - widthToSlide)) {
+            //IS THE CARD BEFORE THIS IN ITS right MOST POSITION OR NOT
+            if (cardSlideVal > (((cardIndex - 1) * perCardShowWidth) - widthToSlide)) {
 
-                /*left = ((f-1)*30)-320;
-                console.log("New Left "+left);
-                a.children("li").eq(f-1).stop().animate({
-                    left: left + "px"
-                    //left: f * 30 + "px"
-                }, 700);*/
+                /*right = ((cardIndex-1)*30)-320;
+                console.log("New right "+right);
+                sliderWrapper.children("li").eq(cardIndex-1).stop().animate({
+                    right: right + "px"
+                    //right: cardIndex * 30 + "px"
+                }, animationTime);*/
 
-                /*a.children("li").eq(f).nextAll().each(function (g) {
+                /*sliderWrapper.children("li").eq(cardIndex).nextAll().each(function (g) {
                 $(this).stop().animate({
-                    left: 500 + f * 30 + g * 30 + "px"
-                    }, 700)
+                    right: 500 + cardIndex * 30 + g * 30 + "px"
+                    }, animationTime)
                 });*/
-                var prevLeft = [];
-                a.children("li").eq(f).prevAll().each(function (g) {
-                    var totalCards = a.children("li").eq(f).prevAll().length;
+                var prevSlideVal = [];
+                sliderWrapper.children("li").eq(cardIndex).prevAll().each(function (g) {
+                    var totalCards = sliderWrapper.children("li").eq(cardIndex).prevAll().length;
                     var affectCard = (totalCards - g - 1);
-                    prevLeft[affectCard] = parseInt($('.card-slider-wrapper').children("li")[affectCard].style.left);
-                    if (prevLeft[affectCard] > ((affectCard * perCardShowWidth) - widthToSlide)) {
-                        prevLeft[affectCard] = (affectCard * perCardShowWidth) - widthToSlide;
-                        console.log(prevLeft[affectCard]);
-                        $(this).stop().animate({
-                            left: prevLeft[affectCard] + "px"
-                        }, 700);
+                    prevSlideVal[affectCard] = parseInt($('.card-slider-wrapper').children("li")[affectCard].style[slideDirection]);
+                    if (prevSlideVal[affectCard] > ((affectCard * perCardShowWidth) - widthToSlide)) {
+                        prevSlideVal[affectCard] = (affectCard * perCardShowWidth) - widthToSlide;
+                        var animationObj = {};
+                        animationObj[slideDirection] = prevSlideVal[affectCard] + "px";
+                        $(this).stop().animate(animationObj, animationTime);
                     };
 
                 });
@@ -82,48 +93,53 @@ $.fn.accordion = function () {
         };
 
         //ALWAY TAKE THIS CARD TO ITS ORIGINAL POSITION AND ALSO ALL IT NEXT ELEMENTS TO THEIR ORIGINAL POSITION
+
         var takeToOriginalPosition = function (card, cardInd) {
-            card.stop().animate({
-                left: (cardInd * perCardShowWidth) + "px"
-            }, 700);
+            var animateObj = {};
+            animateObj[slideDirection] = (cardInd * perCardShowWidth) + "px";
+            card.stop().animate(animateObj, animationTime);
         };
 
-        takeToOriginalPosition(a.children("li").eq(f), f);
-        a.children("li").eq(f).nextAll().each(function (g) {
+        takeToOriginalPosition(sliderWrapper.children("li").eq(cardIndex), cardIndex);
+        sliderWrapper.children("li").eq(cardIndex).nextAll().each(function (g) {
             var card = $(this);
-            takeToOriginalPosition(card, f + g + 1);
+            takeToOriginalPosition(card, cardIndex + g + 1);
         });
 
-        /*a.children("li").eq(f).nextAll().each(function (g) {
+        /*sliderWrapper.children("li").eq(cardIndex).nextAll().each(function (g) {
             $(this).stop().animate({
-                left: 500 + f * 30 + g * 30 + "px"
-            }, 700)
+                right: 500 + cardIndex * 30 + g * 30 + "px"
+            }, animationTime)
         });
-        a.children("li").eq(f).prevAll().each(function (g) {
+        sliderWrapper.children("li").eq(cardIndex).prevAll().each(function (g) {
             $(this).stop().animate({
-                left: (f - g - 1) * 30 + "px"
-            }, 700)
+                right: (cardIndex - g - 1) * 30 + "px"
+            }, animationTime)
         })*/
     };
 
-    a.children("li").mouseenter(function () {
-        var f = a.children("li").index(this);
-        c = f;
-        b(f);
-        clearTimeout(e);
-        d()
+    //DEFINE SLIDING ON HOVER 
+    sliderWrapper.children("li").mouseenter(function () {
+        var index = sliderWrapper.children("li").index(this);
+        c = index;
+        slideToCard(index);
+        clearTimeout(autoSlideTimer);
+        autoSlide();
     });
 
-    function d() {
-        e = setTimeout(function () {
-            if (c >= a.children("li").length) {
+    //METHOD SLIDING THE CARDS AUTOMATICALLY
+    function autoSlide() {
+        autoSlideTimer = setTimeout(function () {
+            if (c >= sliderWrapper.children("li").length) {
                 c = 0
             } else {
                 c++
-            }
-            b(c);
-            d()
-        }, 5000);
-    }
-    d()
+            };
+            slideToCard(c);
+            autoSlide();
+        }, slideDelay);
+    };
+
+    //INITIATE AUTO SLIDING
+    autoSlide()
 };
